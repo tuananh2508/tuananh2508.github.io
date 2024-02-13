1425c5c0dd01a6711a8dfdaaa674f6023f480fd0
Macbook <macbook@TuanAnh-2.local>
2024-02-13 14:21:21 +0700

    DevOps: change server config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/will-you-be-my-valentine",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 8080
-  }
 });
